45865d551707844d4cc47fa773659d9f417961dc
Bunje Kelly <kellybunje@gmail.com>
2024-03-11 11:26:12 -0700

    server

--- server.js
+++ server.js
@@ -3,13 +3,15 @@
 import express from 'express';
 import router from './routes/index';
 
-const app = express();
-const port = process.env.PORT || 5000;
+const port = parseInt(process.env.PORT, 10) || 5000;
 
-app.use('/', routes);
+const app = express();
+
+app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`server running on port ${port}`);
 });
 
-module.exports = app;
+export default app;
